370c9cbb755f6786ec577b77323fe55709b5d38d
jzkm <zbrockij@gmail.com>
2023-12-29 01:10:18 +0100

    file changes

---
+++ boty/bot_jana_2/main.py
@@ -0,0 +1,39 @@
+import chess
+import random
+import sys
+
+class Bot:
+    def __init__(self):
+        self.board = chess.Board()
+        self.tura = False if sys.argv[1]=='b' else True #True - moja tura, False - tura przeciwnika
+        self.licznik = 2
+
+    def make_move(self):
+        move = None
+        while move==None:
+            move = random.choice(list(move.uci() for move in self.board.legal_moves))
+        print(move)
+        self.board.push(chess.Move.from_uci(move))
+        self.tura = False
+            
+
+    def pass_opponent_move(self):
+        move = input()
+        # print(move)
+        self.board.push(chess.Move.from_uci(move))
+        self.tura = True
+        
+
+
+def run():
+    bot = Bot()
+
+    # while bot.board.is_stalemate==False and bot.board.is_checkmate==False:
+    while not(bot.board.is_game_over()):
+        if bot.tura:
+            bot.make_move()
+        else:
+            bot.pass_opponent_move()
+
+
+run()
