15e3b14e206ae992051104fac3d61eefe012a339
qxyzpk <75301132+qxyzpk@users.noreply.github.com>
2023-12-29 16:40:14 +0100

    integracja walki botów z wizualizacją i zmiany w losowym bocie

--- boty/bot_jana_1/main.py
+++ boty/bot_jana_1/main.py
@@ -2,16 +2,35 @@ import chess
 import random
 import sys
 
+
 class Bot:
     def __init__(self):
         self.board = chess.Board()
         self.tura = False if sys.argv[1]=='b' else True #True - moja tura, False - tura przeciwnika
         self.licznik = 2
 
+    def check_stalemate(self, move):
+        self.board.push(move)
+        ret = self.board.is_stalemate()
+        self.board.pop()
+        return not ret
+
     def make_move(self):
         move = None
-        while move==None:
-            move = random.choice(list(move.uci() for move in self.board.legal_moves))
+        l_moves = list(filter(self.check_stalemate, self.board.legal_moves))
+        if not l_moves:
+            l_moves = self.board.legal_moves
+        try:
+            move = random.choice([move.uci() for move in l_moves if self.board.piece_at(move.to_square) is not None])
+        except IndexError:
+            try:
+                move = random.choice([move.uci() for move in l_moves if self.board.piece_at(move.from_square).symbol().lower()=='p'
+                                    and len(move.uci())==5 and move.uci()[-1]=="q"])
+            except IndexError:
+                try:
+                    move = random.choice(list(move.uci() for move in l_moves if self.board.piece_at(move.from_square).symbol().lower()=='p'))
+                except IndexError:
+                    move = random.choice(list(move.uci() for move in l_moves))
         print(move)
         self.board.push(chess.Move.from_uci(move))
         self.tura = False
--- run_duel.py
+++ run_duel.py
@@ -21,11 +21,12 @@ else:
 server = subprocess.Popen(['python','server/main.py'],stdout=subprocess.PIPE)
 client1 = subprocess.Popen(['python','boty/client.py',names[0]],stdout=subprocess.PIPE)
 client2 = subprocess.Popen(['python','boty/client.py',names[1]],stdout=subprocess.PIPE)
-visualization = subprocess.Popen(['python','wizualizacja_gry/display.py'])
+visualization = subprocess.Popen(['python','wizualizacja_gry/display.py'], stdin=subprocess.PIPE, encoding="utf-8")
 
 while True:
     move = server.stdout.readline().decode('utf-8').strip()
     print(move)
+    print(move, file=visualization.stdin, flush=True)
 
 
 
