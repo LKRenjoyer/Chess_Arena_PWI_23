ceba8eda3a20e0574345275cc61aab12f899de37
jzkm <zbrockij@gmail.com>
2023-12-29 21:17:25 +0100

    update run duel

--- run_duel.py
+++ run_duel.py
@@ -30,14 +30,25 @@ if args.eve or (not args.pvp and not args.pvp and not args.pve):
         move = server.stdout.readline().decode('utf-8').strip()
         print(move)
         print(move, file=visualization.stdin, flush=True)
-elif(args.pvp):
+elif(args.pve):
     server = subprocess.Popen(['python','server/main.py'],stdout=subprocess.PIPE)
     client = subprocess.Popen(['python','boty/client.py',args.bot1],stdout=subprocess.PIPE)
     time.sleep(1)
     player = subprocess.Popen(['python','boty/client.py',args.bot2,'--player'],stdout=subprocess.PIPE)
+    # time.sleep(0.5)
     # visualization = subprocess.Popen(['python','wizualizacja_gry/display.py'], stdin=subprocess.PIPE, encoding="utf-8")
+    # while True:
+    #     move = server.stdout.readline().decode('utf-8').strip()
+    #     print(move)
+    #     print(move, file=visualization.stdin, flush=True)
     while True:
         pass
+elif(args.pvp):
+    server = subprocess.Popen(['python','server/main.py'],stdout=subprocess.PIPE)
+    player = subprocess.Popen(['python','boty/client.py',args.bot2,'--player'],stdout=subprocess.PIPE)
+    while True:
+        pass
+
 
 
 
