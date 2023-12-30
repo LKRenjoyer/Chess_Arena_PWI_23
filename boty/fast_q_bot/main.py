9b8dc4e08a3b67190cdbb6779d9b0353d34da636
qxyzpk <75301132+qxyzpk@users.noreply.github.com>
2023-12-30 20:47:42 +0100

    nowy bot i testy do walki botów

---
+++ boty/fast_q_bot/main.py
@@ -0,0 +1,36 @@
+import chess
+import random
+import sys
+
+class Bot:
+    def __init__(self):
+        self.board = chess.Board()
+        self.tura = False if sys.argv[1]=='b' else True #True - moja tura, False - tura przeciwnika
+
+    def make_move(self):
+        move = next(iter(self.board.legal_moves)).uci()
+        print(move)
+        self.board.push(chess.Move.from_uci(move))
+        self.tura = False
+            
+
+    def pass_opponent_move(self):
+        move = input()
+        # print(move)
+        self.board.push(chess.Move.from_uci(move))
+        self.tura = True
+        
+
+
+def run():
+    bot = Bot()
+
+    # while bot.board.is_stalemate==False and bot.board.is_checkmate==False:
+    while not(bot.board.is_game_over()):
+        if bot.tura:
+            bot.make_move()
+        else:
+            bot.pass_opponent_move()
+
+
+run()
