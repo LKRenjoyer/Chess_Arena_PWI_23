9c6c01c8375e9fdce2be37ae1660954ece74aaac
CyprianZ49 <150189300+CyprianZ49@users.noreply.github.com>
2024-01-21 22:24:28 +0100

    działający, prawie wspaniały bot!!!

--- boty/snekEngineButGood/main.py
+++ boty/snekEngineButGood/main.py
@@ -8,20 +8,23 @@ board = chess.Board(sys.argv[2])
 zobrist = Zobrist()
 hash = zobrist.getHash(board)
 skip = 0
-if (board.turn == chess.WHITE and sys.argv[1] == 'b') or (chess.turn == chess.BLACK and sys.argv[1] == 'w'):
+if (board.turn == chess.WHITE and sys.argv[1] == 'b') or (board.turn == chess.BLACK and sys.argv[1] == 'w'):
     skip = 1
 while 1:
     if skip == 0:
+        #print(board)
         move = alphabetaMax(board, -infinity*infinity, infinity*infinity, 0, hash, zobrist)
         print(move.uci(), flush=True)
         hash = zobrist.changeHash(board, hash, move.uci())
         board.push(move)
-        if board.is_checkmate:
+        if board.is_checkmate():
             break
+    #print(board)
+    skip = 0
     move = input()
     move = chess.Move.from_uci(move)
     hash = zobrist.changeHash(board, hash, move.uci())
     board.push(move)
-    if board.is_checkmate:
+    if board.is_checkmate():
         break
 exit(0)
