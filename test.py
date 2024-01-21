17fc941459cdf3aff65ced9ab6d986af3a48f0a5
jzkm <zbrockij@gmail.com>
2024-01-22 00:16:08 +0100

    test bota damiana
    Nie wywala błędu podczas 60 gier

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import shlex
 import subprocess
 
 for i in range(20):
-    out = subprocess.run(shlex.split("python run_duel.py fast_q_bot fast_q_bot -eve -nv"))
+    out = subprocess.run(shlex.split("python run_duel.py test_101bot bot_jana_1_lepszy -eve -nv"))
     print(i)
 
 
