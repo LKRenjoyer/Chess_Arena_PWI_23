a3db9b418f445e63b854d987810a34da25183943
CyprianZ49 <150189300+CyprianZ49@users.noreply.github.com>
2024-01-22 03:06:06 +0100

    snakeOriginal now properly closes

--- boty/snakeOriginal/main.py
+++ boty/snakeOriginal/main.py
@@ -11,9 +11,13 @@ if (chess.turn == 0 and sys.argv[1] == 'b') or (chess.turn == 1 and sys.argv[1] 
     skip = 1
 while 1:
     if skip == 0:
-        move = alphabetaMax(chess, -infinity, infinity, 0)
+        move = alphabetaMax(chess, -infinity*infinity, infinity*infinity, 0)
         print(move, flush=True)
         chess.push(move)
+        if len(chess.getLegalMoves()) == 0:
+            break
     skip = 0
     move = input()
     chess.push(move)
+    if len(chess.getLegalMoves()) == 0:
+        break
